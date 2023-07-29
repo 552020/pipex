10dcc58ef90fd7da2ca2e5e8601b3910120c2971
552020 <stefanolombardo@posteo.de>
2023-07-29 23:53:30 +0200

    some more work

--- src/execve.c
+++ src/execve.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+char	**ft_split(const char *s, char c);
+
 int main(int argc, char **argv, char **envp)
 {
 	int i = 0;
 	const char *cmd;
-	char*	path_env;
+	char	*path_env;
+	char	**path_dirs;
 
 	if (argc < 2) {
 			fprintf(stderr, "Please provide a command to execute.\n");
@@ -28,6 +31,16 @@ int main(int argc, char **argv, char **envp)
 		fprintf(stderr, "PATH environment variable not found\n");
 	}
 	printf("path_env: %s\n", path_env);
+	path_env = path_env + 5;
+	path_dirs = ft_split(path_env, ':');
+	i = 0;
+	while (path_dirs[i])
+	{
+		printf("path_dird[%d]: %s\n", i, path_dirs[i]);
+		i++;
+	}
+	
+
 	return (0);
 }
 
