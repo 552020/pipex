ad387fff992d93ef400af1a97d7557add666fdd0
552020 <stefanolombardo@posteo.de>
2023-07-30 02:03:53 +0200

    mandatory working

--- include/pipex.h
+++ include/pipex.h
@@ -1,2 +1,8 @@
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+int execute_cmd1(char *infile, char *cmd1, char **envp, int *fildes);
+int execute_cmd2(char *outfile, char *cmd2, char **envp, int *fildes);
--- src/utils.c
+++ src/utils.c
@@ -1,53 +1,120 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include "pipex.h"
 
-#define PATH_PREFIX "PATH="
-
-char *find_env_var(char **envp, const char *var)
+char *extract_path_env(char **envp)
 {
-    int i = 0;
-    int var_length = strlen(var);
+	int i = 0;
+	char *path_env;
 
-    while (envp[i])
-    {
-        if (strncmp(envp[i], var, var_length) == 0)
-            return (envp[i] + var_length);
-        i++;
-    }
-    return (NULL);
+	path_env = NULL;
+	while (envp[i])
+	{
+		if(ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path_env = envp[i];
+			break ;
+		}
+		i++;
+	}
+	if (!path_env)
+	{
+		ft_putstr_fd("PATH environment variable not found\n", 2);
+		exit(1);
+	}
+	path_env = path_env + 5;
+	return (path_env);
 }
 
-char *find_path(char *cmd, char **envp)
+char *extract_path(char *path_env, char *cmd)
 {
-    char **paths;
-    char *path;
-    int  i;
-    char *part_path;
-    char *path_env_var = find_env_var(envp, PATH_PREFIX);
+	char **path_dirs;
+	char *full_path;
+	char *tmp_path;
+	int i;
 
-    if (path_env_var == NULL)
-        return NULL; // or return "command not found";
+	i = 0;
 
-    paths = ft_split(path_env_var, ':');
-    i = 0;
-    while (paths && paths[i])
-    {
-        part_path = ft_strjoin(paths[i], "/");
-        path = ft_strjoin(part_path, cmd);
-        free(part_path);
-        if (access(path, F_OK) == 0)
-        {
-            while (paths[++i])
-                free(paths[i]);
-            free(paths);
-            return path;
-        }
-        free(path);
-        i++;
-    }
-    i = -1;
-    while (paths && paths[++i])
-        free(paths[i]);
-    free(paths);
-    return NULL; // or return "command not found";
+
+	path_dirs = ft_split(path_env, ':');
+	while(path_dirs[i])
+	{
+		tmp_path = ft_strjoin(path_dirs[i], "/");
+		full_path = ft_strjoin(tmp_path, cmd);
+		free(tmp_path);
+		if(access(full_path, F_OK) == 0)
+		{
+			i = -1;
+			while(path_dirs[++i])
+				free(path_dirs[i]);
+			free(path_dirs);
+			return (full_path);
+		}
+		free(full_path);
+		i++;
+	}
+	i = -1;
+	while(path_dirs[++i])
+		free(path_dirs[i]);
+	free(path_dirs);
+	return (NULL);
 }
+
+int execute_cmd1(char *infile, char *cmd1, char **envp, int *fildes)
+{
+	char 	**cmd;
+	char	*path_env;
+	char	*path;
+	int 	infile_fildes;
+
+	close(fildes[0]);
+	infile_fildes = open(infile, O_RDONLY);
+	if (infile_fildes < 0)
+	{
+		ft_putstr_fd("Error opening input file\n", 2);
+		exit(1);
+	}
+	dup2(infile_fildes, STDIN_FILENO);
+	close(infile_fildes);
+	dup2(fildes[1], STDOUT_FILENO);
+	close(fildes[1]);
+	cmd = ft_split(cmd1, ' ');
+	path_env = extract_path_env(envp);
+	path = extract_path(path_env, cmd[0]);
+	if (!path)
+	{
+		ft_putstr_fd("Command not found\n", 2);
+		return (1);
+	}
+	execve(path, cmd, envp);
+	return (0);
+}
+
+int execute_cmd2(char *outfile, char *cmd2, char **envp, int *fildes)
+{
+	char 	**cmd;
+	char	*path_env;
+	char	*path;
+	int 	outfile_fildes;
+
+	close(fildes[1]);
+	outfile_fildes = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (outfile_fildes < 0)
+	{
+		ft_putstr_fd("Error opening output file\n", 2);
+		exit(1);
+	}
+	dup2(outfile_fildes, STDOUT_FILENO);
+	close(outfile_fildes);
+	dup2(fildes[0], STDIN_FILENO);
+	close(fildes[0]);
+	cmd = ft_split(cmd2, ' ');
+	path_env = extract_path_env(envp);
+	path = extract_path(path_env, cmd[0]);
+	if (!path)
+	{
+		ft_putstr_fd("Command not found\n", 2);
+		return (1);
+	}
+	execve(path, cmd, envp);
+	return (0);
+}
+
